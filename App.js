90cb5e118a55d2fa0e9f9c5bc903896f010ba559
XNJNP <kolanpoletaev@yandex.ru>
2024-05-10 22:40:49 +0300

    Co-authored-by: L-Arzt <L-Arzt@users.noreply.github.com>

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from 'react-native';
+import News from './components/news/index'
 
 export default function App() {
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <News />
       <StatusBar style="auto" />
     </View>
   );
